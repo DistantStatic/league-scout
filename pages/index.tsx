29179fa5d718f1b628ffebdb8ffba11503fd2b7b
Oscar Rodriguez <distantstatic101@protonmail.com>
2021-11-01 21:06:52 -0400

    background fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import NavBar from '../components/navbar/navbar'
 
 export default function Home() {
     return (
-        <div className="" style={{backgroundImage: "/CelebrationBg_Arcade_2019.png"}}>
+        <div className=" h-screen backdrop-filter bg-no-repeat bg-cover " style={{backgroundImage: "url(/CelebrationBg_Arcade_2019.png)"}}>
             <Head>
                 <title>
                     League Scout
