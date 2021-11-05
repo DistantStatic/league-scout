8b21e740df6d52bb231d2b14148afa9eb6e901c3
Oscar Rodriguez <oscarrodriguez101@gmail.com>
2021-11-05 14:19:15 -0400

    inteface lib created

---
+++ interface-lib/account/account-lib.tsx
@@ -0,0 +1,30 @@
+export interface BuiltResponse {
+    base: AccountResponse,
+    rankedQueues: Array<RankedResponse>
+}
+
+export interface AccountResponse {
+    id: string,
+    accountId: string,
+    puuid: string,
+    name: string,
+    profileIconId: number,
+    revisionDate: number,
+    summonerLevel: number
+}
+
+export interface RankedResponse {
+    leagueId: string,
+    queueType: string,
+    tier: string,
+    rank: string,
+    summonerId: string,
+    summonerName: string,
+    leaguePoints: number,
+    wins: number,
+    losses: number,
+    veteran: boolean,
+    inactive: boolean,
+    freshBlood: boolean,
+    hotStreak: boolean
+}
