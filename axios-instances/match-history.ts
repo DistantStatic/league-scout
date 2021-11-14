5944a843c75b45a6c4eea075ce9ebe20ec33bc38
Oscar Rodriguez <distantstatic101@protonmail.com>
2021-11-13 20:30:58 -0500

    lets not log api keys...

--- axios-instances/match-history.ts
+++ axios-instances/match-history.ts
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 const RIOT_API_KEY = process.env.RIOT_API_KEY
 
-console.log(RIOT_API_KEY)
-
 const instance = axios.create({
     baseURL: "https://americas.api.riotgames.com/lol/",
     headers: {
